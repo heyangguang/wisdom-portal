6f0cddac8c596f97a89ca1476444e2641b96e8ad
heyang <heyangev@cn.ibm.com>
2020-05-16 21:45:29 +0800

    更新接口文档服务器地址

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host 127.0.0.1:8080
+// @host 192.168.31.2:8080
 
 // @securityDefinitions.apikey ApiKeyAuth
 // @in header
