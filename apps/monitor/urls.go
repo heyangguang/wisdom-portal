19ee4952c712277d10c03facf3c1599449f79fe6
heyang <heyangev@cn.ibm.com>
2020-06-28 15:11:59 +0800

    优化URL位置

--- apps/monitor/urls.go
+++ apps/monitor/urls.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func Routers(routeGroup *gin.RouterGroup) {
 	routeGroup.GET("/monitor/tcp", getTcpMonitor)
 	routeGroup.GET("/monitor/tcp/quality", getTcpMonitorQuality)
+	routeGroup.GET("/monitor/intermediate", queryIntermediateMonitor)
 	routeGroup.POST("/monitor/tcp", addMonitor)
 	routeGroup.POST("/monitor/intermediate", addIntermediateMonitor)
-	routeGroup.GET("/monitor/intermediate", queryIntermediateMonitor)
 }
